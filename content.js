4501b79fd0717d2e4586af31f58d9b0aad57628a
Kiyoshic <79597433+Kiyoshic@users.noreply.github.com>
2021-03-31 18:07:25 +0300

    Fix some bugs, optimize the performance.

--- content.js
+++ content.js
@@ -2,40 +2,17 @@ var url_regex = /^(?:http(?:s|)):\/\/(?:www\.|)discord\.com(?:\/|)$/g,
 	condition = url_regex.test(location.href) ? 1 : url_regex.test(location.href.replace('login/', '').replace('login', '')) ? 2 : false;
 
 if (condition) {
-	function getElemByClassName(className, callback) {
-		if (![...document.querySelectorAll('*')].some(elem => {
-			if (String(elem.className ?? '').includes(className)) {
-				console.log(elem);
-				callback(elem);
-				return true;
+	let elemQueue = {};
+	document.addEventListener('DOMNodeInserted', function(e) {
+		for (let key of Object.keys(elemQueue)) {
+			if (e.className.includes(key)) {
+				elemQueue[key](e);
 			}
-			return false;
-		})) {
-			callback(null);
 		}
-	}
-
-	function waitFor(className, callback, timeout) {
-		getElemByClassName(className, element => {
-			if (element) {
-				callback(element);
-			} else {
-				if (timeout) {
-					return window.setTimeout(function () {
-						return window.requestAnimationFrame(function () {
-							waitFor(className, callback);
-						});
-					}, timeout);
-				}
-				return window.requestAnimationFrame(function () {
-					waitFor(className, callback);
-				});
-			}
-		});
-	}
+	}, false);
 
 	function login(token, className, color=null) {
-		getElemByClassName(className, ctx => {
+		elemQueue[className] = ctx => {
 			if (ctx) {
 				if (color) {
 					ctx.style.color = color;
@@ -48,25 +25,25 @@ if (condition) {
 			setTimeout(() => {
 				location.reload();
 			}, 2500);
-		});
+		};
 	}
 
 	if (condition === 1) {
-		waitFor('appButton', target => {
-				const a = document.createElement('a');
-				a.className = target.children[0].className;
-				a.onclick = event => {
-					const input = prompt('Enter token here');
-					if (input) {
-						login(input, 'h1', 'var(--not-quite-black)');
-					}
-				};
-				a.innerHTML = 'Enter token';
-				a.style['margin-right'] = '2%';
-				target.appendChild(a);
-			}, 100);
+		elemQueue['appButton'] = target => {
+			const a = document.createElement('a');
+			a.className = target.children[0].className;
+			a.onclick = event => {
+				const input = prompt('Enter token here');
+				if (input) {
+					login(input, 'h1', 'var(--not-quite-black)');
+				}
+			};
+			a.innerHTML = 'Enter token';
+			a.style['margin-right'] = '2%';
+			target.appendChild(a);
+		};
 	} else if (condition === 2) {
-		waitFor('mainLoginContainer', target => {
+		elemQueue['mainLoginContainer'] = target => {
 			target = target.children[target.children.length - 1];
 			const a = document.createElement('a');
 			let n = target.children[target.children.length - 1].children;
@@ -79,6 +56,6 @@ if (condition) {
 			};
 			a.innerHTML = 'Enter token';
 			target.appendChild(a);
-		}, 100);
+		};
 	}
 }
